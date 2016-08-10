5baca937a8a85fdf0048704a8c22c82a3730b9fe
kevteg <kevin.hernandez@unet.edu.ve>
2016-08-10 17:03:58 -0430

    Improving the serial selection and program usage

--- serial_server.py
+++ serial_server.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
 import time
 import serial
 import socket
 import threading
 import struct
+import os
+import argparse
 '''
     Author: Keeeevin
 '''
 class serial_server():
-    def __init__(self, port, bauds, receive_multicast_group, receive_udp_port, send_multicast_group, send_udp_port):
+    def __init__(self, _port, bauds, receive_multicast_group, receive_udp_port, send_multicast_group, send_udp_port):
+        port = os.popen("./findusb | grep " + _port + " | egrep -o '^[^-]+'").read().rstrip()
         #Setting udp socket to send information that comes through serial:
         self.send_multicast_group = send_multicast_group
         self.send_udp_port = send_udp_port
@@ -23,10 +28,15 @@ class serial_server():
         mreq = struct.pack("4sl", socket.inet_aton(receive_multicast_group), socket.INADDR_ANY)
         self.receive_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
         #Serial:
-        self.ser = serial.Serial(
-            port=port,
-            baudrate=bauds
-        )
+        try:
+            self.ser = serial.Serial(
+                port=port,
+                baudrate=bauds
+            )
+        except:
+            print("There has been an error with that device. Are you sure it exists?")
+            exit(-1)
+
     def run(self):
         print("Starting serial server")
         self.dowork = True
@@ -63,5 +73,16 @@ class serial_server():
 #TODO:
 #Set udp server to receive
 #Run script to find XBee port name
-serv = serial_server("/dev/ttyACM0", 9600, "230.2.2.2", 6000, "230.1.1.1", 5000)
+parser = argparse.ArgumentParser(prog='serialserver', usage='%(prog)s [options]',description='Script to have several connections to one serial.')
+parser.add_argument('-p','--port',dest='serial_port',type=str, required = True, help='Serial port to connect in')
+parser.add_argument('-b','--bauds',dest='bauds',type=int, default=9600, help='Serial bauds')
+parser.add_argument('-r','--receive',dest='receive_multicast',type=str, default="230.2.2.2", help='Multicast address to receive data')
+parser.add_argument('-o','--receive_port',dest='receive_port',type=int, default=6000, help='Multicast port to receive data')
+parser.add_argument('-s','--send',dest='send_multicast',type=str, default="230.1.1.1", help='Multicast address to send data')
+parser.add_argument('-i','--send_port',dest='send_port',type=int, default=5000, help='Multicast port to send data')
+
+args = parser.parse_args()
+
+#serv = serial_server("/dev/ttyACM0", 230400, "230.2.2.2", 6000, "230.1.1.1", 5000)
+serv = serial_server(args.serial_port, args.bauds, args.receive_multicast, args.receive_port, args.send_multicast, args.send_port)
 serv.run()
